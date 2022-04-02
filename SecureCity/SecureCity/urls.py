0984f0717b7e2fcbd56b8e05e6e4b28c5a80873a
yinonh <yinon.h21@gmail.com>
2022-04-02 23:24:30 +0300

    initiate

--- SecureCity/SecureCity/urls.py
+++ SecureCity/SecureCity/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from HomePage import views as HomePageV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageV.home),
 ]
