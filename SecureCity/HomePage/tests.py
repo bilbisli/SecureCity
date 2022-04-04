246057bca3307b00fb8b1b3b454ecdeb4a0c97b9
yinonh <yinon.h21@gmail.com>
2022-04-04 23:19:33 +0300

    BSPM2022T11-62

--- SecureCity/HomePage/tests.py
+++ SecureCity/HomePage/tests.py
@@ -1,8 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from HomePage import views
 
 class AnimalTestCase(TestCase):
     def setUp(self):
-        pass
+        self.factory = RequestFactory()
 
     def test_animals_can_speak(self):
         self.assertEqual('hello', 'hello')
+
+    def test_homePage(self):
+        request = self.factory.get('')
+        response = views.home(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.status_code, 404)
+
+
