59a316025aa0aced68ca7ffe31702640f3497d30
yinonh <yinon.h21@gmail.com>
2022-04-13 21:22:33 +0300

    BSPM2022T11-60

--- SecureCity/AdminRequest/models.py
+++ SecureCity/AdminRequest/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-MAX_STRING = 1023
+MAX_STRING = 150
 
 class AdminRequest(models.Model):
     userAsked = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
     date = models.DateField('date', default=timezone.localtime(timezone.now()), null=False)
     description = models.TextField('description', max_length=MAX_STRING, null=True, blank=True)
+
+    @classmethod
+    def create(cls, currentUser, description):
+        UserReuest = cls(userAsked=currentUser, description=description)
+        return UserReuest
