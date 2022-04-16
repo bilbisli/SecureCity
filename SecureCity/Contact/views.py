75296035e4ceacacd71099e32ef2130b95b5f432
shaywe@ac.sce.ac.il <shaywe@ac.sce.ac.il>
2022-04-17 00:13:40 +0300

    BSPM2022T11-123

--- SecureCity/Contact/views.py
+++ SecureCity/Contact/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 from .models import Contact
+from Authentication.models import Parent
 
 
 def contact_management(request):
     contacts = Contact.objects.all()
+    patrols = Parent.objects.filter(is_patrol_manager=True)
     context = {
-            'contacts': contacts
+            'contacts': contacts,
+            'patrols': patrols
     }
     return render(request, 'HomePage/ContactPage.html', context)
