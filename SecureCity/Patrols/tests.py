e37b7d99a66a5c155f51a5ecd2e2ff23c6458596
Israel Avihail <bilbisli@gmail.com>
2022-04-27 04:03:32 +0300

    added coverage results command to jenkins + tests for patrols management (mostly csv report download) to raise coverage above 60%

--- SecureCity/Patrols/tests.py
+++ SecureCity/Patrols/tests.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse, HttpResponseNotFound
+import csv
+import io
+
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.test import TestCase, RequestFactory, Client
 from django.contrib.auth.models import User
 from django.urls import reverse, resolve
@@ -14,14 +17,18 @@ class PatrolManagementTest(TestCase):
         self.user.save()
         self.factory = RequestFactory()
         self.client = Client()
+        self.patrol_params = {'title': 'testPatrol', 'location': 'A', 'priority': '1',
+                              'manager': self.user, 'date': '2022-01-01', 'start_time': '12:00', 'end_time': '13:00'}
+        self.patrol = Patrol.objects.create(**self.patrol_params)
+        self.patrol.save()
         # assure login
         logged_in = self.client.login(username='testerFinal', password='testpassword')
         self.assertTrue(logged_in)
 
     def tearDown(self):
-        user = User.objects.get(id=self.user.pk)
+        self.patrol.delete()
         self.client.logout()
-        user.delete()
+        self.user.delete()
 
     def test_patrolManagement_not_manager(self):
         # test patrol management when user is not manager
@@ -53,6 +60,34 @@ class PatrolManagementTest(TestCase):
         self.assertEqual(resolver.view_name, 'PatrolManagement')
         self.assertEqual(resolver.func, patrol_management)
 
+    def test_patrolManagement_csv_download(self):
+        self.user.profile.is_patrol_manager = True
+        self.user.profile.save()
+        self.user.save()
+        self.assertTrue(self.user.profile.is_patrol_manager)
+        self.assertTrue(self.user.is_authenticated)
+        lst = {"ToCSV": [self.patrol.id,]}
+        response = self.client.post(reverse('PatrolManagement'), lst)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.__class__, HttpResponse)
+        # check that the file is downloaded
+        self.assertEqual(response.get('Content-Disposition'), 'attachment; filename=Patrols_Summary.csv')
+        # check that the file is a csv file
+        self.assertEqual(response.get('Content-Type'), 'text/csv')
+        # check the content of the csv file
+        content = response.content.decode('utf-8')
+        cvs_reader = csv.reader(io.StringIO(content))
+        body = list(cvs_reader)
+        headers = body.pop(0)
+        self.assertEqual(headers, ['', 'Title', 'Location', 'Priority', 'Manager', 'Date', 'Time'])
+        self.assertEqual(body[0], ['0',
+                                   self.patrol.title,
+                                   self.patrol.location,
+                                   str(self.patrol.priority),
+                                   self.patrol.manager.username,
+                                   self.patrol.date,
+                                   f'{self.patrol.start_time}:00-{self.patrol.end_time}:00'])
+
     def test_patrolManagement_add_patrol(self):
         # self.client.logout()
         self.user.profile.is_patrol_manager = True
@@ -72,26 +107,27 @@ class PatrolPageTest(TestCase):
         self.user.save()
         self.factory = RequestFactory()
         self.client = Client()
+        # create a patrol
+        self.patrol = Patrol.objects.create(title='testPatrol', description='testDescription', manager=self.user)
+        self.patrol.save()
         # assure login
         logged_in = self.client.login(username='testerFinal', password='testpassword')
         self.assertTrue(logged_in)
 
     def tearDown(self):
-        user = User.objects.get(id=self.user.pk)
+        self.patrol.delete()
         self.client.logout()
-        user.delete()
+        self.user.delete()
 
     def test_patrol_page(self):
-        # create a patrol
-        patrol = Patrol.objects.create(title='testPatrol', description='testDescription', manager=self.user)
         # response = self.client.get('/patrols/PatrolManagement/PatrolPage/1')
         # test patrol page
-        url = reverse('patrol_page', args=(patrol.pk,))
-        self.assertEqual(url, f'/patrols/patrol/{patrol.pk}/')
+        url = reverse('patrol_page', args=(self.patrol.pk,))
+        self.assertEqual(url, f'/patrols/patrol/{self.patrol.pk}/')
         response = self.client.get(url, follow=True)
         resolver = resolve(url)
         # test url is of the post
-        self.assertEqual(url, f'/patrols/patrol/{patrol.pk}/')
+        self.assertEqual(url, f'/patrols/patrol/{self.patrol.pk}/')
         # check that the user (which is a manager) arrives to the patrol management page
         self.assertNotEqual(response.status_code, 404)
         self.assertNotEqual(response.status_code, 302)
