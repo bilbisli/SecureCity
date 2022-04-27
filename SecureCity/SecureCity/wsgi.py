27d71b4c82a7b23f4fc0cc30a8291d1f0d230cb6
Israel Avihail <bilbisli@gmail.com>
2022-04-27 05:04:47 +0300

    fixing jenkins build tests after coverage 12

--- SecureCity/SecureCity/wsgi.py
+++ SecureCity/SecureCity/wsgi.py
@@ -8,27 +8,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SecureCity.settings')
-
-# set environment variable to tell django where your settings.py is
-os.environ['DJANGO_SETTINGS_MODULE'] = 'SecureCity.settings'
-
-application = get_wsgi_application()
-
-
-
-
-path = './SecureCity/SecureCity'
-
-if path not in sys.path:
-    sys.path.append(path)
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SecureCity.settings")
 
 application = get_wsgi_application()
