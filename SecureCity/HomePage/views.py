b04f1d136baf339ec90706a6b18c898caf7016ce
Shay Weitzman <shaywe@ac.sce.ac.il>
2022-05-17 23:16:48 +0300

    try

--- SecureCity/HomePage/views.py
+++ SecureCity/HomePage/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 #import pandas as pd
+from django.contrib.auth.models import User
 
 
 @login_required(login_url='/Login/')
 def home(request):
+    if not User.objects.filter(is_superuser=True).first():
+        user = User.objects.create(
+            username='admin',
+            is_superuser=True,
+        )
+        user.set_password('123456')
+        user.save()
     return render(request, 'HomePage/homePage.html')
