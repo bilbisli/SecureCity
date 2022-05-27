ea3c4cbcd982233093a6c5a0d6fa317c38ff1121
Israel Avihail <bilbisli@gmail.com>
2022-05-27 20:59:55 +0300

    some tweaks in base + others

--- SecureCity/Authentication/views.py
+++ SecureCity/Authentication/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import ExtendedUserCreationForm, ParentProfileForm
 from Patrols import models as PatrolModels
-from adminPage.models import get_data
+from adminPage.models import get_data, updateData
 
 
 crime_columns = (
@@ -61,6 +61,7 @@ def loginU(request):
         )
         user.set_password('123456')
         user.save()
+        updateData()
     if request.method == 'GET':
         return render(request, 'Authentication/Login.html', {'form': AuthenticationForm(), 'error': ""})
     else:
@@ -74,39 +75,6 @@ def loginU(request):
 
 
 def residentPage(request):
-#     headers = []
-#     data = []
-#     fields = []
-#     objects = []
-#     df = pd.DataFrame()
-#     neighborhood_column = "neighborhood_1"
-#     heb_neighb_col = "שכונה"
-#     total_population_col = "סה''כ"
-#     columns = [heb_neighb_col, total_population_col] + list(crime_columns)
-#     df = get_data('unified_data')
-#     if df is not None:
-#
-#         if not request.user.is_superuser:
-#             df = df[df[neighborhood_column] == request.user.profile.Neighborhood]
-#
-#         df[heb_neighb_col] = df[neighborhood_column]
-#         df = df.drop(neighborhood_column, axis=1)
-#         df = df[columns]
-#         df = df.groupby([heb_neighb_col]).sum()
-#         # df['neighborhood_1'] = df.index
-#         df.to_excel('static/data.xlsx')
-#
-#     p_type = "patrol"
-#     context = {
-#         'objects': objects,
-#         'fields': fields,
-#         'type': p_type,
-#         'data': data,
-#         'headers': headers,
-#         'df': df
-#     }
-#     return render(request, 'Authentication/residentPage.html', context)
-# =======
     patrols = PatrolModels.Patrol.objects.filter(approved_reactions__profile__user__id=request.user.id)
     fields = PatrolModels.Patrol._meta.get_fields()[:-3]
     context = {
@@ -114,8 +82,6 @@ def residentPage(request):
         'fields': fields
     }
     if request.user.profile.is_patrol_manager:
-
-        df = pd.DataFrame()
         neighborhood_column = "neighborhood_1"
         heb_neighb_col = "שכונה"
         total_population_col = "סה''כ"
@@ -130,8 +96,7 @@ def residentPage(request):
             df = df.drop(neighborhood_column, axis=1)
             df = df[columns]
             df = df.groupby([heb_neighb_col]).sum()
-            # df['neighborhood_1'] = df.index
-            df.to_excel('static/data.xlsx')
+            # df.to_excel('static/data.xlsx')
 
         objects = PatrolModels.Patrol.objects.filter(manager=request.user)
         p_type = "patrol"
@@ -141,4 +106,4 @@ def residentPage(request):
             'df': df
         }
         context.update(context2)
-        return render(request, 'Authentication/residentPage.html', context)
+    return render(request, 'Authentication/residentPage.html', context)
